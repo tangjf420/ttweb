d8a14a9761ae962039e03ae3e5372f6adb65f9c7
tangjf <tangjf@skong.com>
2017-05-03 11:49:31 +0800

    新增基础类

---
+++ zMbatisGenerator/src/main/java/com/tangjf/base/BaseMapper.java
@@ -0,0 +1,16 @@
+package com.tangjf.base;
+
+import java.util.List;
+
+public interface BaseMapper<T> {
+
+    T get(String id);
+
+    List<T> queryList(T bean);
+
+    int insert(T bean);
+
+    int update(T bean);
+
+    int delete(T bean);
+}
